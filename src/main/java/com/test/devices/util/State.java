0ce6f805b91c32c6536e808f1220b0773aa2d1b6
Andrelima92 <andre.carrasqueira.lima@gmail.com>
2026-03-31 12:02:44 +0100

    state enum
    has return and create to serialize how my constants are

--- src/main/java/com/test/devices/util/State.java
+++ src/main/java/com/test/devices/util/State.java
@@ -1,7 +1,27 @@
 package com.test.devices.util;
 
-public enum Status {
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+import com.test.devices.exceptions.InvalidStateException;
+
+import java.util.Arrays;
+
+public enum State {
     AVAILABLE,
     IN_USE,
-    INACTIVE
+    INACTIVE;
+    
+    @JsonValue
+    public String toJson(){
+        return this.name().toLowerCase().replace("_","-");
+    }
+
+    @JsonCreator
+    public static State fromString(String value){
+        return Arrays.stream(State.values())
+                .filter(s->s.name().equalsIgnoreCase(value.replace("-","_")))
+                .findFirst().orElseThrow(()->
+                        new InvalidStateException(value + " is not a valid state"));
+    }
+    
 }
