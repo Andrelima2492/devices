51209210f390580210722c3d9de93012c4c9a13e
Andrelima92 <andre.carrasqueira.lima@gmail.com>
2026-03-31 12:03:39 +0100

    added filtering to the repository

--- src/main/java/com/test/devices/repository/DeviceRepository.java
+++ src/main/java/com/test/devices/repository/DeviceRepository.java
@@ -1,4 +1,12 @@
 package com.test.devices.repository;
 
-public interface DeviceRepository {
+import com.test.devices.model.Device;
+import com.test.devices.util.State;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface DeviceRepository extends JpaRepository<Device, Integer> {
+    List<Device> findDevicesByBrand(String brand);
+    List<Device> findDevicesByState(State state);
 }
