f88e801565abc6f811949c7d5fbe36ddff30c1ad
Andrelima92 <andre.carrasqueira.lima@gmail.com>
2026-03-31 12:01:42 +0100

    dependencies and model.
    Added a way to convert input to my constant format and return it

--- src/main/java/com/test/devices/model/Device.java
+++ src/main/java/com/test/devices/model/Device.java
@@ -1,5 +1,37 @@
 package com.test.devices.model;
 
 
+
+import com.test.devices.util.State;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+@Data
+@NoArgsConstructor
 public class Device {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @NotNull
+    @NotBlank
+    private String name;
+    @NotNull
+    @NotBlank
+    private String brand;
+    @Enumerated(EnumType.STRING)
+    private State state;
+    @NotNull
+    private  Integer creationTime;
+
+    public Device(String name, String brand, State state, Integer creationTime){
+        this.name=name;
+        this.brand=brand;
+        this.state=state;
+        this.creationTime=creationTime;
+    }
 }
