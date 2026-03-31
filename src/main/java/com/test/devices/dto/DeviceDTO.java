0420b9b8e140c51c1bbf8b8d59595c18192f1b5e
Andrelima92 <andre.carrasqueira.lima@gmail.com>
2026-03-31 12:03:07 +0100

    Created DTO to pass objects from services

--- src/main/java/com/test/devices/dto/DeviceDTO.java
+++ src/main/java/com/test/devices/dto/DeviceDTO.java
@@ -1,4 +1,15 @@
 package com.test.devices.dto;
 
+import com.test.devices.util.State;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class DeviceDTO {
+    private Integer id;
+    private String name;
+    private String brand;
+    private State state;
+    private Integer creationTime;
 }
